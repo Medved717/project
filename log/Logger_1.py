abc97dd64e2f95ec55b5612a8c85421152012fe2
Your Name <you@example.com>
2026-04-23 15:38:57 +0300

    Потренировался с обычным логированием

--- log/Logger_1.py
+++ log/Logger_1.py
@@ -1,12 +1,17 @@
 import logging
 
-
 logger = logging.getLogger(__name__)
 file_handler = logging.FileHandler('example.txt', encoding='utf-8')
 file_formatter = logging.Formatter('%(asctime)s, %(levelname)s: %(message)s')
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 logger.setLevel(logging.DEBUG)
+
+# logger.debug('Сообщение Debug')
+# logger.info('Сообщение Info')
+# logger.warning('Сообщение Warning')
+# logger.error('Сообщение Error')
+# logger.critical('Сообщение Critical')
 
 def abc(a, b):
     if a > b:
@@ -16,3 +21,10 @@ def abc(a, b):
         c = a - b
         logger.debug('Условие не пройдено')
     return c
+
+print(abc(3,2))
+print(abc(6,12))
+print(abc(7,32))
+print(abc(8,5))
+print(abc(35,1))
+print(abc(8,0))
